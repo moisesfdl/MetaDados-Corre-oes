64e9d149d917b5e9ab1eb061a0820224031557bc
moises ferreira de lima <43420973+moisesfdl@users.noreply.github.com>
2022-10-28 13:19:43 -0300

    Create Prolabore.js

---
+++ Prolabore.js
@@ -0,0 +1,79 @@
+/**
+ *'©moises ferreira de lima'
+ * @param {MetaDados&Correçao} prolabore 
+ * @returns 
+ */
+//#body...moisesfdl
+function cartorio(){
+function contabilidade(prolabore){
+    //#body...bankofamerica
+    //#body...interactivebrokers
+    var console = 'moisesfdl'
+    var uri = 'https://www.gov.br/trabalho-e-previdencia/pt-br'
+    uri = 'https://banco.bradesco/html/pessoajuridica/net-empresa/index.shtm'
+    return `\t\t\t\tProlabore Contabil!!\n\n\t\tMatriz:\t\tMetaDados&Correçao\n\n\t\tProprietario:\tMoises ferreira de lima\n${prolabore}`
+}
+console.log(contabilidade(`\n\t\tSocio:\tEmpresa Individual\t\tRg: 27.950.938-8\n\n\tCnpj: 35.669.326/0001-90\t Renda Anual: R$20.000,00 `
++`\n\tCpf: 275.551.158-32\tRenda Folha de Salario: R$6.250,00\n\tAno 2021\n\t`
++`Renda Janeiro  : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Janeiro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Fevereiro: \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Fevereiro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Março    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Março: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Abril    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Abril: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Maio     : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Maio: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Junho    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Junho: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Julho    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Julho: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Agosto   : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Agosto: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Setembro : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Setembro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Outubro  : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Outubro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Novembro : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Novembro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Desembro : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Desembro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\n\t`
++`Fechamento Ano : 2021\t Renda Anual : R$ 20.040,00 reais \n\t`
++`Fechament/Imposto 2021 : R$1002 - R$20040 \n\t`
++`Fechamento 2021 : Renda Total: R$19.038,00\n\t`
++`\n\tAno: 2022\n\t`
++`Renda Janeiro  : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Janeiro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Fevereiro: \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Fevereiro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Março    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Março: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Abril    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Abril: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Maio     : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Maio: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Junho    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Junho: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Julho    : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Julho: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Agosto   : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Agosto: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Setembro : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Setembro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Outubro  : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Outubro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Novembro : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Novembro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\t`
++`Renda Desembro : \t\tR$1.670,00 * 12 = Renda Anual : R$20.040,00 / 12\n\t`
++`Imposto Desembro: %0,05% = 1670 * 0,05 = 83,50 :Pay Imposto : R$83,50 \n\n\t`
++`Fechamento Ano : 2022\t Renda Anual : R$ 20.040,00 reais \n\t`
++`Fechament/Imposto 2022 : R$1002 - R$20040 \n\t`
++`Fechamento 2022 : Renda Total: R$19.038,00\n\n\t`
++`\n\t\tMetaDados&Correçao\n\t\tASSINATURA: Moises ferreira de lima`
++`\n\t\tDat: 01/12/2019\tLocal: SJCampos-SP\n\n\t`
++`Rodape:Rua:Paturi;Numero:230;Cidade:SaoJoseDosCampos;Estado:SaoPaulo;\n\t`
++`Pais:Brasil;\tCep:12214563\tCelular:5512-996497466\n\t`
++`Contato:.email:\tmoisesfdl_pbt@hotmail.com.br;\tmoisesprobabilidade@gmail.com;`))
+}
+cartorio()
