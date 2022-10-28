d3e6e284b644c45942c206b20db9274d3a52c86c
moises ferreira de lima <43420973+moisesfdl@users.noreply.github.com>
2022-10-28 13:37:51 -0300

    Create Redesin.js

---
+++ Redesin.js
@@ -0,0 +1,111 @@
+/**
+ * © moises ferreira de lima
+ * @param {cnpj} redesin 
+ * @returns 
+ */
+function redesin(){
+function cnpj(redesin){
+    //#body...redesin
+    AbstractRange: globalThis
+    AbortController: cnpj
+    return `\tMetaDados&Correçao\tCnpj: 35.669.326/0001-90\n\t${redesin}`
+}
+console.log(cnpj(`Nome: moises ferreira de lima\tRg: 27.950.938-8\n\t`
++`Redesin\n\t\tComprovante De Incriçao\n\tCADASTRO NACIONAL DA PESSOA JURIDICA\tRef Data: 05/10/2022\n`
++`|---------------------------------------------------------------------------------|\n`
++`| NUMERO DE INSCRIÇAO | COMPROVANTE DE INSCRIÇAO E DE SITUAÇAO | DATA DE ABERTURA |\n`
++`| 35.669.326/0001-90  |                 CADASTRAL              |   01/12/2019     |\n`
++`|  Matriz             |                                        |                  |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| NOME EMRPESARIAL                                                                |\n`
++`|  MOISES FERREIRA DE LIMA 27555115832                                            |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| TITULO DE ESTABELECIMENTO (NOME DE FANTASIA)                 |     PORTE        |\n`
++`|  METADADOS&CORREÇOES                                         |      ME          |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| NOME DO REPRESENTANTE LEGAL                | CPF             |  QUALIFICAÇAO    |\n`
++`|   MOISES FERREIRA DE LIMA                  | 275.551.158-32  |  Emrpesario      |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| CODIGO E DESCRIÇAO DA ATIVIDADE ECONOMICA PRINCIPAL                             |\n`
++`| 5819-1/00 - Ediçao de cadastros, listas e de outros produtos graficos           |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| CODIGO E DESCRIÇAO DAS ATIVIDADES ECONOMICAS SECUNDARIAS                        |\n`
++`| 7420-0/04 - Filmagem de festas e eventos                                        |\n`
++`| 9511-8/00 - Reparaçao e manutençao de computadores e de equipamentos perifericos|\n`
++`| 3319-8/00 - Manutençao e reparaçao de equipamentos e produtos nao especificados |\n`
++`| anteriormente                                                                   |\n`
++`| 8219-9/01 - Fotocopias                                                          |\n`
++`| 3240-0/99 - Fabricaçao de outros brinquedos e jogos recreativos nao             |\n`
++`| especificados anteriormente                                                     |\n`
++`| 8599-6/03 - Treinamento em informatica                                          |\n`
++`| 8219-9/99  - Preparaçao de documentos e serviços especializados de apoio        |\n`
++`| administrativo nao especificados anteriormente                                  |\n`
++`| 7420-0/01 - Atividade de produçao de fotografias , exceto aerea e submarina     |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| CODIGO E DESCRIÇAO DA NATUREZA JURIDICA                                         |\n`
++`| 213-5  - Empresario  (Individual)                                               |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| LOGRADOURO                         | NUMERO  |  COMPLEMENTO                     |\n`
++`|   PATURI                           |  230    |                                  |\n`
++`|---------------------------------------------------------------------------------|\n`
++`| CEP             |BAIRRO/DISTRITO     | MUNICIPIO                       |   UF   |\n`
++`| 12214563        | Buquirinha2/SJC-SP |  SAO JOSE DOS CAMPOS            |  SP    |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|  ENDEREÇO ELETRONICO                          |   TELEFONE                      |\n`
++`|   moisesfdl_pbt@hotmail.com.br                |   ( 12 ) 996497466              |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|  ENTE FEDERATIVO RESPONSAVEL (EFR)                                              |\n`
++`|     Responsavel legal MOISES FERREIRA DE LIMA                                   |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|  SITUAÇAO CADASTRAL                               |  DATA DA SITUAÇAO CADASTRAL |\n`
++`|    ATIVA                                          |       01/12/2019            |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|  MOTIVO DE SITUAÇAO CADASTRAL                                                   |\n`
++`|  Formalizaçao de renda variavel      ( INVESTIDOR )                             |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|   SITUAÇAO ESPECIAL                        | DATA DA SITUAÇAO ESPECIAL          |\n`
++`|      NADA CONSTA                           |        01/12/2019                  |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|                        ASSINATURA DO RESPONSAVEL                                |\n`
++`|               MOISES FERREIRA DE LIMA  RG : 27.950.938-8                        |\n`
++`|---------------------------------------------------------------------------------|\n`
++`|   QUADRO DE SOCIOS E ADMINISTRADORES  (NAO HA INFORMAÇAO)                       |\n`
++`|      APENAS O RESPONSAVEL                                                       |\n`
++`|---------------------------------------------------------------------------------|\n`))
+const irpf = function(){
+    var Redesin = `cnpj: 35.669.326/0001-90`
+    for(let declare of Redesin){
+        declare = [1]
+        return declare
+    }
+}
+console.log(irpf())
+}
+redesin()
+function MetaDadosCorreçao(){
+    const Empresa = function(){
+        var Individual = `moises ferreira de lima 27555115832`
+        if (Individual) {
+            this.Individual
+        }
+        return Individual
+    }
+    console.log(Empresa())
+    const Socio = function(){
+        var Empresa = `Moises ferreira de lima\nRg: 27.950.938-8\nNao tenho socio\n`
+        if (Empresa) {
+            this.Empresa
+        }
+        return Empresa
+    }
+    console.log(Socio())
+    const ResponsavelLegal = function(){
+        var Classificaçao = `MetaDados&Correçoes\nEmpresario Executivo-> Moises ferreira de lima\nRg: 27.950.938-8\nCtps: Numero-> 77646 Serie-> 0150\n`
+        if (Classificaçao) {
+            this.Classificaçao
+        }
+        return Classificaçao
+    }
+    console.log(ResponsavelLegal())
+}
+MetaDadosCorreçao()
