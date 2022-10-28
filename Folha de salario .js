c56746ef0d5e6312fe766393d7c2c51f3f060b44
moises ferreira de lima <43420973+moisesfdl@users.noreply.github.com>
2022-10-28 13:32:12 -0300

    Create Folha de salario .js

---
+++ Folha de salario .js
@@ -0,0 +1,28 @@
+/**
+ * ©moises ferreira de lima
+ * @param {MetaDados&Correçao} FolhaSalario 
+ * @returns 
+ */
+//#body...moisesfdl
+function cartorio(){
+function EmpresaMetaDadosCorrecao(FolhaSalario){
+     //#body...bankofamerica
+    //#body...interactivebrokers
+    var console = 'moisesfdl'
+    var uri = 'https://www.gov.br/trabalho-e-previdencia/pt-br'
+    uri = 'https://banco.bradesco/html/pessoajuridica/net-empresa/index.shtm'
+    return `\tMatrix: MetaDados&Correçao\t\tFolha Salario\n\tCnpj: 35.669.326/0001-90\t\tRef: DD/MM/YYYY\n\t${FolhaSalario}`
+}
+console.log(EmpresaMetaDadosCorrecao(`Carteira: 77646\t\tFunçao: Programador&Corretor\n\t`
++`|Cod/Pag|\tDescriçao Pag|\tValorPag\t|Descontos|\n\t\t\tcorretagem\t\t\t0,00`
++`\n\t\t\tProgramador\t\t\t0,00\n\t\t\testudo correçao\t\t\t0,00\n\t\t\tProdutor Live Game\t\t0,00`
++`\n\t\t\tBlogueiro\t\t\t0,00\n\t\t\tEscritor Poema Blog\t\t0,00\n\t\t\tVendedor camisa c/desenho\t0,00\n\t\t\tManutençao Micro\t\t0,00`
++`\n\t\t\tMinerador Cripto Bytes e coin\t0,00\n\t------------------------------------------------------------\n\t`
++`bank: Bradesco | agencia: 0225 | Conta: 040054-8 |\n\t\t\tBase Total Receber: R$6.250,00\n\t---------------------------------------------------------\n\t`
++`base Salario: R$6.250,00 base FGTS: 312,50 base IRPF: 62,50\n\tbase ICMS: 312,50\n\t`
++`b/Sal - b/Fgts | b/Sal - b/Irpf | b/Sal - Icms |\n\t`
++`| 6250 - 312,5 | 6250 - 62,50  | 6250 - 312,50 |\n\t`
++`|Ass: Moises Ferreira de lima: | dat: /09/2022 | \n\t`
++`\t\tTotal Base Salario Receber: R$5.562,50`))
+}
+cartorio()
