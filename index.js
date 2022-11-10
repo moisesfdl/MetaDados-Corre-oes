a7444f10a29cc27a22624304c52a690fbba23b6e
moises ferreira de lima <43420973+moisesfdl@users.noreply.github.com>
2022-11-09 21:46:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+function html(){
+    const ol = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(ol())
+    const table = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(table())
+    const div = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(div())
+    const tr = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(tr())
+    const td = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(td())
+    const head = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(head())
+    const body = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(body())
+    const meta = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(meta())
+    const title = function(){
+        var funçao = `$0.01*1/12`
+        if (funçao) {
+            globalThis:funçao
+        }
+        return funçao
+    }
+    console.log(title())
+}
+module.id = html()
